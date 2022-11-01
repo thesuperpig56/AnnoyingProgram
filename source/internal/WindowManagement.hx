f08b0d7d3d805297c4a7b41884b62ea2fc14ad48
thesuperpig56 <97651743+thesuperpig56@users.noreply.github.com>
2022-10-31 20:05:44 -0700

    Started to work on OpenFL windows.

--- source/internal/WindowManagement.hx
+++ source/internal/WindowManagement.hx
@@ -1,22 +1,35 @@
 package internal;
 
-import openfl.Lib;
-import flixel.graphics.FlxGraphic;
-import openfl.display.Bitmap;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.graphics.FlxGraphic;
 import lime.app.Application;
 import lime.graphics.RenderContext;
-import lime.ui.MouseButton;
 import lime.ui.KeyCode;
 import lime.ui.KeyModifier;
+import lime.ui.MouseButton;
 import lime.ui.Window;
+import openfl.Lib;
+import openfl.display.Bitmap;
+import openfl.display.Sprite;
 import openfl.geom.Matrix;
 import openfl.geom.Rectangle;
-import openfl.display.Sprite;
 import openfl.utils.Assets;
 
 class WindowManagement
 {
+    private var windows:Array<FuckedWindow> = [];
     
+}
+
+class FuckedWindow
+{
+	public var window:Window;
+	public var name:String;
+	public var sprite:Sprite;
+
+	public function new()
+    {
+
+    }
 }
